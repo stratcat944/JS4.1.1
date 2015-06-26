d1e596d33ed3fb1bbaf733d317b461ebbd8cad79
Steve Domenico <stratcat944@gmail.com>
2015-06-26 16:29:03 -0600

    added 4.1.1.4

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 $(document).on('ready', function(){
-	$("button").on("click", function() {
-	console.log("This a message that will be printed to the console.");
+// 	$("button").on("click", function() {
+// 	console.log("This a message that will be printed to the console.");
+// });
+
+$(".msg-text").on("click", function() {
+	$("body").append('<p>This is a message that will be printed in the body.</p>')
+	$("body").append('<p>Holy shit I did it!</p>')
 });
 
-
-
+$(".h1-msg-text").on("click", function() {
+	$("body").append('<h1>This is "some text to the body element".</h1>')
+});
 
 
 
